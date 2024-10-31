8c11913240471375d3476225bc4d25b89edb65c1
narges afzali <153275951+kotarou-katsura@users.noreply.github.com>
2024-10-31 12:19:45 +0330

    make: methods and properties
    add error_index_b (for binary code like 1001) ,error_index_d (for decimal number like 9) fields.add the find_error method.

--- 5.C.py
+++ 5.C.py
@@ -4,6 +4,8 @@ class Hamming:
         self.get_code()
         self.parity_value = []
         self.parity_position = []
+        self.error_index_b = 0
+        self.error_index_d = 0
         self.list_parity_positions()
         self.find_parity_value()
 
@@ -28,6 +30,23 @@ class Hamming:
                     value += self.code[j + i]
             self.parity_value.append((value - self.code[parity - 1]) % 2)
 
+    def find_error(self):
+        for i in range(len(self.parity_position) - 1, -1, -1):
+            if self.code[self.parity_position[i] - 1] != self.parity_value[i]:
+                self.error_index_b += 1
+            if i != 0:
+                self.error_index_b *= 10
+        self.error_index_d = int(str(self.error_index_b), 2)
+        print(f"the error find in position {self.error_index_d}")
+        print("the corrected code is:", end=" ")
+        print("the corrected code is:", end=" ")
+        for i in range(len(self.code)):
+            if i == self.error_index_b - 1:
+                if self.code[i] == 0:
+                    self.code[i] = 1
+                else:
+                    self.code[i] = 0
+            print(self.code[i], end="")
 
 def is_parity(n):
     if n == 1:
@@ -39,4 +58,6 @@ def is_parity(n):
             return False
     else:
         return False
-
+h1=Hamming()
+h1.print_parity()
+h1.find_error()
